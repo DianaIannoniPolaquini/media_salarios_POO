47d317a8af2270d471e5f8d5adafd43c61818f9b
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-12 14:26:58 -0300

    primeiro commit

---
+++ MEDIA_SALARIO_POO/FUNCIONARIO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEDIA_SALARIO_POO
+{
+    internal class FUNCIONARIO
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ MEDIA_SALARIO_POO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace MEDIA_SALARIO_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FUNCIONARIO f1 = new FUNCIONARIO();
+            FUNCIONARIO f2 = new FUNCIONARIO();
+            
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine());
+
+            double media = (f1.Salario + f2.Salario) / 2;
+            Console.WriteLine($"O salário médio é {media.ToString("F2", CultureInfo.InvariantCulture)}.");
+        }
+    }
+}
